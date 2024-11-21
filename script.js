7ba5c8b3393e5c67ea2230d547d4e7598dee8602
jonathan-kevin <114663162+jonathan-kevin@users.noreply.github.com>
2024-11-21 07:59:36 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,66 @@
+var title = $("<title>Jonathan</title>");
+
+var style = $("<link>", {
+	rel: "stylesheet",
+	href: "./src/style/screen.template.css?version=#",
+});
+
+var fontawesome = $("<link>", {
+	rel: "stylesheet",
+	href: "./src/fontawesome/css/all.css",
+});
+
+var favicon = $("<link>", {
+	rel: "icon",
+	type: "image/png",
+	size: "16x16",
+	href: "./src/img/favicon.png",
+});
+
+$("head").append(style, fontawesome, favicon, title);
+function reloadCSS() {
+	const links = document.getElementsByTagName("link");
+
+	Array.from(links)
+		.filter((link) => link.rel.toLowerCase() === "stylesheet" && link.href)
+		.forEach((link) => {
+			const url = new URL(link.href, location.href);
+			url.searchParams.set("forceReload", Date.now());
+			link.href = url.href;
+		});
+}
+
+$(document).keydown(function (event) {
+	if (event.altKey && event.keyCode === 82) {
+		event.preventDefault();
+		reloadCSS();
+	}
+});
+
+$(document).ready(function () {
+	var theToggler = $("<button>", {
+		id: "theToggler",
+		text: "Standard",
+	});
+
+	$("body").addClass("saPc saLargeScreen saStandard").append(theToggler);
+
+	function toggleClasses() {
+		if ($("body").hasClass("saStandard")) {
+			$("body").removeClass("saStandard").addClass("saCompact saPc saLargeScreen");
+			$("#theToggler").text("Compact");
+		}
+		else if ($("body").hasClass("saCompact")) {
+			$("body").removeClass("saCompact saPc saLargeScreen").addClass("saSmallScreen");
+			$("#theToggler").text("SmallScreen");
+		}
+		else {
+			$("body").removeClass("saSmallScreen").addClass("saStandard saPc saLargeScreen");
+			$("#theToggler").text("Standard");
+		}
+	}
+
+	$("#theToggler").click(function () {
+		toggleClasses();
+	});
+});
